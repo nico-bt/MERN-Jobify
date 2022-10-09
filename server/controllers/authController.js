7850f0841899b2994a053953590fa7b721b1f97d
Nico-Battaglia <nico-battaglia@hotmail.com>
2022-10-09 02:08:14 -0300

    Add validator to check email format

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,26 @@
-const register = (req, res) => {
-    res.send("Register user")
+const validator = require('validator');
+const User = require('../models/user');
+
+
+const register = async (req, res) => {
+    const {name, email, password, location} = req.body
+    
+    if(!name || !email || !password) {
+        return res.status(400).json({msg: "Please enter all fields"})
+    }
+    
+    if (!validator.isEmail(email)) {
+        return res.status(400).json({msg: "Please enter a valid email"})
+    }
+
+    try {
+        const newUser = await User.create({name, email, password, location})
+        res.status(201).json(newUser)
+    } catch (error) {
+        console.log(error)
+        res.status(400).json(error)
+
+    }
 }
 
 const login = (req, res) => {
