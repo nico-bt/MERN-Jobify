d3a5a26a3bf71172ff4573e329971962b4e3387b
Nico-Battaglia <nico-battaglia@hotmail.com>
2022-10-16 00:45:35 -0300

    Job Model and createJob controller

--- server/controllers/jobsController.js
+++ server/controllers/jobsController.js
@@ -1,18 +1,46 @@
-const createJob = (req, res) => {
-    res.json("Create a Job");
+const Job = require("../models/Job")
+
+
+// CREATE a Job
+//---------------------------------------------------------------------
+const createJob = async (req, res) => {
+    const { position, company } = req.body
+    
+    if (!position || !company) {
+        res.status(400).json("Please provide all values")
+    }
+    
+    // userId comes from auth middleware
+    req.body.createdBy = req.userId
+    
+    const job = await Job.create(req.body)
+    res.status(201).json({ job })
 }
 
+
+// DELETE a Job
+//---------------------------------------------------------------------
 const deleteJob = (req, res) => {
     res.json({job: "Delete a Job" })
 }
 
-const getAllJobs = (req, res) => {
-    res.json({job: "Get all jobs" })
+// GET ALL Jobs
+//---------------------------------------------------------------------
+const getAllJobs = async (req, res) => {
+    try {
+        const jobs = await Job.find({createdBy:req.userId})
+        res.json(jobs)
+    } catch (error) {
+        console.log(error);   
+    }
 }
 
+// UPDATE Job
+//---------------------------------------------------------------------
 const updateJob = (req, res) => {
     res.json({job: "Update a Job" })
 }
+
 
 const showStats = (req, res) => {
     res.json({job: "Show stats" })
