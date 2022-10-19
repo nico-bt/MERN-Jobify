e37ee1c7f43834699b58a79cb40d59d03c95cd66
Nico-Battaglia <nico-battaglia@hotmail.com>
2022-10-19 00:29:43 -0300

    Delete unused values/imports to avoid warnings

--- client/src/components/Navlinks.js
+++ client/src/components/Navlinks.js
@@ -1,9 +1,7 @@
 import { NavLink } from 'react-router-dom'
-import { useAppContext } from '../context/appContext'
 import links from '../utils/links'
 
 const NavLinks = ({ toggleSidebar }) => {
-  const {clearValues} = useAppContext()
   return (
     <div className='nav-links'>
       {links.map((link) => {
@@ -16,11 +14,6 @@ const NavLinks = ({ toggleSidebar }) => {
             onClick={toggleSidebar}
             className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}
             end={true}
-            // To delete Edit state values in context, and clear the form values for Add Job link
-            // onClick={() =>{ 
-            //   if(path=="add-job") { clearValues() 
-            //   }
-            // }}
           >
             <span className='icon'>{icon}</span>
             {text}
